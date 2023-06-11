abcb03edbf13f275a593fc1e0059bb18d29fc319
aakj2010 <aakj2010@gmail.com>
2023-06-11 17:37:54 +0530

    fix:cors error

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const port = process.env.PORT || 5000
 // Middleware
 app.use(express.json());
 app.use(cors({
-    origin: "*",
+    origin: "https://drfazl.netlify.app/",
     credentials: true
 }));
 
