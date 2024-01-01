223805c9d098bcfc5c314a6b3e9de25230ed7229
aakj2010 <aakj2010@gmail.com>
2024-01-01 14:08:47 +0530

    Frontend url added

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ dotenv.config({ path: path.join(__dirname, ".env") })
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: "*",
+    origin: ["https://drfazl.netlify.app"],
     credentials: true
 }))
 
